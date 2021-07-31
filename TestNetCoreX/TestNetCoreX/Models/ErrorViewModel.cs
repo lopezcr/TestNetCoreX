8dbb32ad87ac12d2130402738fcea8309fce8abf
Rafa <Fallo_kof@hotmail.com>
2021-07-31 16:39:06 -0500

    Creacion proyecto

---
+++ TestNetCoreX/TestNetCoreX/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestNetCoreX.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
